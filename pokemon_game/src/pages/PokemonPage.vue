120c4ea83b9b5072c8d0560c9dcf4b32403baead
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2023-12-05 16:08:09 -0500

    Componente Picture

---
+++ pokemon_game/src/pages/PokemonPage.vue
@@ -0,0 +1,17 @@
+<template>
+<h1>¿Quién es este pokémon</h1>
+<PokemonPicture :pokemonId="4" :showPokemon="true"/>
+<PokemonOptions/>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+
+export default {
+  components: { PokemonOptions,PokemonPicture },
+
+}
+</script>
+
